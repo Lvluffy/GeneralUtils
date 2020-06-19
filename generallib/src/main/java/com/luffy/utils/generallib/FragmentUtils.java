d4e03a0bb1701b175a6fd0fed9d94a36c83ed7e4
lvlufei <lvlufei@xiaomi.com>
2020-06-19 10:33:36 +0800

    FragmentUtils扩展

--- generallib/src/main/java/com/luffy/utils/generallib/FragmentUtils.java
+++ generallib/src/main/java/com/luffy/utils/generallib/FragmentUtils.java
@@ -2,6 +2,7 @@ package com.luffy.utils.generallib;
 
 
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 
 /**
@@ -21,25 +22,27 @@ public class FragmentUtils {
         private static final FragmentUtils mFragmentUtils = new FragmentUtils();
     }
 
-    /**
-     * 替换碎片
-     *
-     * @param activity 上下文对象
-     * @param target   目标对象
-     */
     public void replaceFragment(android.app.Activity activity, android.app.Fragment target) {
         replaceFragment(activity, android.R.id.content, target);
+    }
+
+    public void replaceFragment(android.app.Activity activity, int resId, android.app.Fragment target) {
+        replaceFragment(activity, resId, target, null);
     }
 
     /**
      * 替换碎片
      *
-     * @param activity 上下文对象
-     * @param resId    目标ID
-     * @param target   目标对象
+     * @param activity      上下文对象
+     * @param resId         目标ID
+     * @param target        目标对象
+     * @param fragmentExtra 参数接口传参
      */
-    public void replaceFragment(android.app.Activity activity, int resId, android.app.Fragment target) {
+    public void replaceFragment(android.app.Activity activity, int resId, android.app.Fragment target, FragmentExtra fragmentExtra) {
         if (activity == null) return;
+        if (fragmentExtra != null) {
+            target.setArguments(fragmentExtra.putExtra());
+        }
         //碎片管理器
         android.app.FragmentManager manager = activity.getFragmentManager();
         //碎片事务
@@ -50,25 +53,27 @@ public class FragmentUtils {
         transaction.commitAllowingStateLoss();
     }
 
-    /**
-     * 替换碎片
-     *
-     * @param activity 上下文对象
-     * @param target   目标对象
-     */
     public void replaceFragment(android.support.v4.app.FragmentActivity activity, android.support.v4.app.Fragment target) {
         replaceFragment(activity, android.R.id.content, target);
+    }
+
+    public void replaceFragment(android.support.v4.app.FragmentActivity activity, int resId, android.support.v4.app.Fragment target) {
+        replaceFragment(activity, resId, target, null);
     }
 
     /**
      * 替换碎片
      *
-     * @param activity 上下文对象
-     * @param resId    目标ID
-     * @param target   目标对象
+     * @param activity      上下文对象
+     * @param resId         目标ID
+     * @param target        目标对象
+     * @param fragmentExtra 参数接口传参
      */
-    public void replaceFragment(android.support.v4.app.FragmentActivity activity, int resId, android.support.v4.app.Fragment target) {
+    public void replaceFragment(android.support.v4.app.FragmentActivity activity, int resId, android.support.v4.app.Fragment target, FragmentExtra fragmentExtra) {
         if (activity == null) return;
+        if (fragmentExtra != null) {
+            target.setArguments(fragmentExtra.putExtra());
+        }
         //碎片管理器
         android.support.v4.app.FragmentManager manager = activity.getSupportFragmentManager();
         //碎片事务
@@ -79,27 +84,30 @@ public class FragmentUtils {
         transaction.commitAllowingStateLoss();
     }
 
-    /**
-     * 替换碎片
-     *
-     * @param fragment 上下文对象
-     * @param target   目标对象
-     */
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     public void replaceFragment(android.app.Fragment fragment, android.app.Fragment target) {
         replaceFragment(fragment, android.R.id.content, target);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    public void replaceFragment(android.app.Fragment fragment, int resId, android.app.Fragment target) {
+        replaceFragment(fragment, resId, target, null);
+    }
+
     /**
      * 替换碎片
      *
-     * @param fragment 上下文对象
-     * @param resId    目标ID
-     * @param target   目标对象
+     * @param fragment      上下文对象
+     * @param resId         目标ID
+     * @param target        目标对象
+     * @param fragmentExtra 参数接口传参
      */
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
-    public void replaceFragment(android.app.Fragment fragment, int resId, android.app.Fragment target) {
+    public void replaceFragment(android.app.Fragment fragment, int resId, android.app.Fragment target, FragmentExtra fragmentExtra) {
         if (fragment == null) return;
+        if (fragmentExtra != null) {
+            target.setArguments(fragmentExtra.putExtra());
+        }
         //碎片管理器
         android.app.FragmentManager manager = fragment.getChildFragmentManager();
         //碎片事务
@@ -110,25 +118,27 @@ public class FragmentUtils {
         transaction.commitAllowingStateLoss();
     }
 
-    /**
-     * 替换碎片
-     *
-     * @param fragment 上下文对象
-     * @param target   目标对象
-     */
     public void replaceFragment(android.support.v4.app.Fragment fragment, android.support.v4.app.Fragment target) {
         replaceFragment(fragment, android.R.id.content, target);
+    }
+
+    public void replaceFragment(android.support.v4.app.Fragment fragment, int resId, android.support.v4.app.Fragment target) {
+        replaceFragment(fragment, resId, target, null);
     }
 
     /**
      * 替换碎片
      *
-     * @param fragment 上下文对象
-     * @param resId    目标ID
-     * @param target   目标对象
+     * @param fragment      上下文对象
+     * @param resId         目标ID
+     * @param target        目标对象
+     * @param fragmentExtra 参数接口传参
      */
-    public void replaceFragment(android.support.v4.app.Fragment fragment, int resId, android.support.v4.app.Fragment target) {
+    public void replaceFragment(android.support.v4.app.Fragment fragment, int resId, android.support.v4.app.Fragment target, FragmentExtra fragmentExtra) {
         if (fragment == null) return;
+        if (fragmentExtra != null) {
+            target.setArguments(fragmentExtra.putExtra());
+        }
         //碎片管理器
         android.support.v4.app.FragmentManager manager = fragment.getChildFragmentManager();
         //碎片事务
@@ -137,5 +147,9 @@ public class FragmentUtils {
         transaction.replace(resId, target, target.getClass().getName());
         //提交
         transaction.commitAllowingStateLoss();
+    }
+
+    public interface FragmentExtra {
+        Bundle putExtra();
     }
 }
