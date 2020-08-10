ff132336bf738389f1bc233e771a7fbcb8ced67c
lvlufei <lvlufei@xiaomi.com>
2020-08-10 10:11:42 +0800

    新增工具类：BundleUtils

---
+++ generallib/src/main/java/com/luffy/utils/generallib/BundleUtils.java
@@ -0,0 +1,39 @@
+package com.luffy.utils.generallib;
+
+import android.os.Bundle;
+
+/**
+ * Created by lvlufei on 2020-08-10
+ *
+ * @name Bundle-辅助工具
+ */
+public class BundleUtils {
+
+    private BundleUtils() {
+
+    }
+
+    public static BundleUtils getInstance() {
+        return BundleUtilsHolder.instance;
+    }
+
+    private static class BundleUtilsHolder {
+        private static final BundleUtils instance = new BundleUtils();
+    }
+
+    public String printBundle(Bundle bundle) {
+        if (bundle == null) {
+            return null;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String key : bundle.keySet()) {
+            Object value = bundle.get(key);
+            if (value != null && value instanceof Bundle) {
+                stringBuilder.append(printBundle((Bundle) value));
+            } else {
+                stringBuilder.append(key).append("=").append(value).append(",");
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
