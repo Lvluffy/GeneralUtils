9db66de18467e6e198ac288c7c6459203d712c06
lvlufei <lvlufei@xiaomi.com>
2020-11-25 17:06:51 +0800

    新增：DownloadPictureUtils

---
+++ bitmaplib/src/main/java/com/luffy/utils/bitmaplib/DownloadPictureUtils.java
@@ -0,0 +1,57 @@
+package com.luffy.utils.bitmaplib;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by lvlufei on 2020-08-17
+ *
+ * @name 下载图片-辅助工具
+ */
+public class DownloadPictureUtils {
+    private DownloadPictureUtils() {
+    }
+
+    public static DownloadPictureUtils getInstance() {
+        return DownloadPictureUtilsHolder.instance;
+    }
+
+    private static class DownloadPictureUtilsHolder {
+        private static final DownloadPictureUtils instance = new DownloadPictureUtils();
+    }
+
+    /**
+     * 下载
+     *
+     * @param path
+     * @return
+     */
+    public Bitmap download(String path) {
+        Bitmap bitmap = null;
+        InputStream inputStream = null;
+        try {
+            URL url = new URL(path);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                inputStream = connection.getInputStream();
+                bitmap = BitmapFactory.decodeStream(inputStream);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return bitmap;
+    }
+}
